9c284751b0427f859e15d4c385868531bac5a7ce
Randall Harris Haynes <harrishaynes@gmail.com>
2018-03-08 01:27:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    (:word1..:word5).to_a.collect{|word| tmp[word]}.join(" ")+"."
+    (:word1..:word5).to_a.collect{|word| params[word]}.join(" ")+"."
   end
   
   get '/:operation/:number1/:number2' do
